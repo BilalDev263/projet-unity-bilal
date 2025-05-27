403e89a1b8d564ff4afddfd9550778bb6fc7d1bf
unknown <bilal.mezrai@ecole-isitech.fr>
2025-05-27 12:16:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/message', (req, res) => {
+  const name = req.body.name || 'Visiteur';
+  const message = req.body.message || 'Pas de message';
+  res.send(`<h2>Merci ${name} !</h2><p>Votre message : ${message}</p><a href="/">Retour</a>`);
+});
+
+app.listen(3000, () => {
+  console.log('App en ligne sur http://localhost:3000');
+});
